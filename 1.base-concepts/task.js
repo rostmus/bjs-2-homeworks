8430a3d677486124e24e25661664e381a4a0e90c
Ростислав Козлов <rostislavkozlov@MacBook-Air-Rostislav.local>
2022-02-04 23:26:28 +0300

    коммит 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,12 +1,30 @@
+'use strict'
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let D = b**2 - 4*a*c;
+  if (D === 0) {
+    let x = -b/(2*a)
+    arr.push(x)
+  } else if (D > 0) {
+    let x1, x2
+    x1 = (-b + Math.sqrt(D) )/(2*a)
+    x2 = (-b - Math.sqrt(D) )/(2*a)
+    arr.push(x1, x2)
+  } else {
+    arr = []
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+function calculateTotalMortgage(percent, contribution, amount, date) {    //процентная ставкаэ, сумма первоначального взноса, сумма кредита и срок
   let totalAmount;
-
+  let S = amount - contribution // тело кредита
+  let now = new Date() //новая дата
+  let n = date.getFullYear()*12 + date.getMonth() - now.getFullYear()*12 - now.getMonth()
+  let P = percent/(12*100)// (1/12 процентной ставки)
+  let check = S*(P + (P/((Math.pow((1 + P), n)-1))))
+   totalAmount = Number((check * n).toFixed(2))
   // код для задачи №2 писать здесь
 
   return totalAmount;
