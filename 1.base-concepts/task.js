7b5211c320a84e85eacce2d2953645055f843f18
Rostmus <rostmus_mavr@mail.ru>
2022-02-05 16:33:12 +0300

    второй коммит первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,6 @@
 'use strict'
 function solveEquation(a, b, c) {
-  let arr = [];
+  let arr = []
   let D = b**2 - 4*a*c;
   if (D === 0) {
     let x = -b/(2*a)
@@ -10,14 +10,20 @@ function solveEquation(a, b, c) {
     x1 = (-b + Math.sqrt(D) )/(2*a)
     x2 = (-b - Math.sqrt(D) )/(2*a)
     arr.push(x1, x2)
-  } else {
-    arr = []
+  } else { 
+    return arr
   }
   // код для задачи №1 писать здесь
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {    //процентная ставкаэ, сумма первоначального взноса, сумма кредита и срок
+  for (let i = 0; i < arguments.length; i ++) {
+    let z = arguments[i]
+    if (typeof z !== 'string' && typeof z !== 'number') {
+  console.log(`Параметр z содержит неправильное значение ${z}`)
+    }
+  }
   let totalAmount;
   let S = amount - contribution // тело кредита
   let now = new Date() //новая дата
