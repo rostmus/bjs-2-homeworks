777a276c7dd8260e23d7a97f7408c05edca6e5c7
Rostmus <rostmus_mavr@mail.ru>
2022-02-09 20:20:22 +0300

    задание два первый коммит

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -6,12 +6,14 @@ function getArrayParams(arr) {
   sum = 0
   for (let i = 0; i < arr.length; i++) {
     let element = arr[i]
-    if (element < element[i - 1]) {
+    if (element > max) {
       max = element
+    } else if (element < min ) {
+      min = element
     }
     sum += element
   }
-  avg = sum / arr.length
+  avg = Number((sum / arr.length).toFixed(2))
   // Ваш код
 
   return { min: min, max: max, avg: avg };
@@ -19,23 +21,39 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
 
-  // Ваш кода
-  // for ...
-  
+
+function makeWork(arrOfArr, func) {
+  let max = 0;
+  for (let j = 0; j < arrOfArr.length; j++) {
+    if (worker2(arrOfArr[j]) > max) {
+      max = worker2(arrOfArr[j])
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+let dif = 0
+let min = arr[0]
+let max = arr[0]
+ for (let i = 0; i < arr.length; i++) {
+ let particle = arr[i]
+ if(particle > max) {
+   max = particle
+ } else if(particle < min) {
+   min = particle
+ }
+ }
+ dif = Math.abs(max - min)
+ return dif
 }
