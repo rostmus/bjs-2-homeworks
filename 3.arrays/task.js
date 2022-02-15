ac425f48cb5622e516533830ddc41b1364f419eb
Ростислав Козлов <rostislavkozlov@MacBook-Air-Rostislav.local>
2022-02-15 11:46:56 +0300

    первый коммит задание три

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,23 @@
-function compareArrays(arr1, arr2) {
-  let result;
 
+
+function compareArrays(arr1, arr2) {
+  let result = (arr1.length === arr2.length && arr1.every(function(number, index) {
+    return number === arr2[index]
+  }))
+  
   // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(function(number) {
+    return number > 0
+  }).filter(function(number){
+   return number % 3 === 0 
+  }).map(function(number) {
+    return number * 10
+  });
 
   // Ваш код
 
