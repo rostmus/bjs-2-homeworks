36da7c78ee05aa2199d025b9a3bf2e874d2a29e8
Ростислав Козлов <rostislavkozlov@MacBook-Air-Rostislav.local>
2022-03-24 20:41:42 +0300

    новый коммит задание 3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -3,10 +3,10 @@ function cachingDecoratorNew(func) {
   let cashe = []
   function wrapper(... args) {
     const hash = args.join(',')
-    let idx = cashe.findIndex((item)=>item.hash === hash)
-    if (idx !== -1) {
-      console.log('Из кэша: ' + cashe[idx].result)
-      return 'Из кэша: ' + cashe[idx].result
+    let idx = cashe.find((item)=>item.hash === hash)
+    if (idx != undefined ) {
+      console.log('Из кэша: ' + idx.result)
+      return 'Из кэша: ' + idx.result
     } 
     let result = func(...args)
     cashe.push({hash, result })
@@ -17,9 +17,6 @@ function cachingDecoratorNew(func) {
     return 'Вычисляем: ' + result
   }
   return wrapper
-
-  // Ваш код
-  
 }
 
 
@@ -48,22 +45,18 @@ function debounceDecoratorNew(func, ms) {
 
 
 function debounceDecorator2(func, ms) {
-  wrapper.history = 0
+  wrapper.quantity = 0
   let timeout
   let flag = false
   function wrapper(...args) {
+    wrapper.quantity +=1
     if(flag === false) { 
       func(...args)
-      wrapper.history +=1
       flag = true
       timeout = setTimeout(() => {
         flag = false
       }, ms)
-    } else {
-        wrapper.history +=1
-      //   clearTimeout(timeout)
-      //   timeout = setTimeout(() => {flag = false}, ms)
-      }
+    } 
 
     }
 
